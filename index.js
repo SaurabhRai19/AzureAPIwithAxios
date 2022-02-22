fc7c7861c8335f023e8a453d36d28d499193aa8b
Saurabh Rai <42387065+SaurabhRai19@users.noreply.github.com>
2022-02-22 11:05:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,44 @@
+const axios = require("axios");
+let fs = require("fs");
+var writeableStream = fs.createWriteStream("output.json");
+require("dotenv").config();
 
-const axios= require("axios")
-let fs = require('fs')
-var writeableStream = fs.createWriteStream('output.json');
-require('dotenv').config()
-
-async function hello(){
-const accessToken = process.env.AZURE_DEVOPS_TOKEN;
-let orgName='DevOps-MBU';
-let projName= 'DemoProject';
-if (accessToken == null || accessToken === '') {
-  throw new Error('Please provide an access token');
-} else {
-  console.log('token is present!');
-}
-let url= `https://dev.azure.com/${orgName}/${projName}/_apis/pipelines/78/runs?api-version=7.1-preview.1`;
-const request = await axios({
+async function azureApiCall() {
+  const accessToken = process.env.AZURE_DEVOPS_TOKEN;
+  let organization = "DevOps-MBU";
+  let project = "DemoProject";
+  let pipelineId = 78 || null;
+  let runId = 971 || null;
+  if (accessToken == null || accessToken === "") {
+    throw new Error("Please provide an access token");
+  } else {
+    console.log("token is present!");
+  }
+  let url = `https://dev.azure.com/${organization}/${project}/_apis/pipelines?api-version=7.1-preview.1&=`;
+  const request = await axios({
     url,
     headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-        // This!
-        Authorization: `Basic ${Buffer.from(`PAT:${accessToken}`).toString('base64')}`,
-        'X-TFS-FedAuthRedirect': 'Suppress',
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      // This!
+      Authorization: `Basic ${Buffer.from(`PAT:${accessToken}`).toString(
+        "base64"
+      )}`,
+      "X-TFS-FedAuthRedirect": "Suppress",
     },
-});
-let responsedata = request.data;
-const jsonFormat= JSON.stringify(responsedata);
-console.log(jsonFormat);
-writeableStream.write(jsonFormat, 'UTF8');
-writeableStream.end();
-writeableStream.on('finish',function(){
-	console.log('write completed');
-});
+  });
+  let responsedata = request.data;
+  const jsonFormat = JSON.stringify(responsedata, null, 2);
+  console.log(jsonFormat);
+  writeableStream.write(jsonFormat, "UTF8");
+  writeableStream.end();
+  writeableStream.on("finish", function () {
+    console.log("write completed");
+  });
 }
-hello()
+azureApiCall();
+
+// Gets a run for a particular pipeline- https://dev.azure.com/${organization}/${project}/_apis/pipelines/${pipelineId}/runs/${runId}?api-version=7.1-preview.1
+// Gets top 10000 runs for a particular pipeline- https://dev.azure.com/${orgName}/${projName}/_apis/pipelines/${pipelineId}/runs?api-version=7.1-preview.1
+// Get all the pipelines in a project- https://dev.azure.com/${organization}/${project}/_apis/pipelines?api-version=7.1-preview.1&=
+// For azure api calls - https://docs.microsoft.com/en-us/rest/api/azure/devops/?view=azure-devops-rest-7.1
